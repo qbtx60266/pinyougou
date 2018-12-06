a9e6e64fbf67bb11c6e7d76b7d353587049798ba
FallingSkies <635025373@qq.com>
2018-12-06 10:23:40 +0800

    0.0.3 品牌测试代码

---
+++ pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,26 @@
+package com.pinyougou.manager.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author FallingSkies
+ * @date 2018/12/6 10:18
+ */
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll(){
+        return brandService.findAll();
+    }
+}
