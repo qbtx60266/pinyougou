d89aa2ba92894a75ed281b3867fc5c808b966580
FallingSkies <635025373@qq.com>
2018-12-10 20:37:59 +0800

    0.0.972 新增前后端-显示登录名

---
+++ pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.pinyougou.manager.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * 与登陆有关
+ * @author FallingSkies
+ * @date 2018/12/10 20:18
+ */
+
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    /**
+     * 获取登录名
+     * @return
+     */
+    @RequestMapping("/name")
+    public Map name(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map map = new HashMap<>();
+        map.put("loginName",name);
+        return map;
+    }
+}
