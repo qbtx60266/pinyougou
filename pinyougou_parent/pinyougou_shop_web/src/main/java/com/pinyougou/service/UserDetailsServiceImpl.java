a0a015f2bbb00b97d57ed44d4b6bbebab0d041c8
FallingSkies <635025373@qq.com>
2018-12-11 14:30:44 +0800

    0.0.9834 新增后端-商家简单登陆功能完善

--- pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.service;
 
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -18,11 +20,28 @@ import java.util.List;
  * @date 2018/12/11 13:06
  */
 public class UserDetailsServiceImpl implements UserDetailsService{
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         List<GrantedAuthority> grantAuths = new ArrayList<>();
         grantAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
-        return new User(username,"123456",grantAuths);
+
+        TbSeller seller = sellerService.findOne(username);
+        if (seller != null){
+            if (seller.getStatus().equals("1")){
+                return new User(username,seller.getPassword(),grantAuths);
+            }else {
+                return null;
+            }
+        }else {
+            return null;
+        }
     }
 }
