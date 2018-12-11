245bcff04302db58e2ea20cc967cf9a4c7a34900
FallingSkies <635025373@qq.com>
2018-12-11 23:37:41 +0800

    0.0.98451 新增前端-类型的删除功能

--- pinyougou_parent/pinyougou_manager_web/src/main/webapp/js/controller/itemCatController.js
+++ pinyougou_parent/pinyougou_manager_web/src/main/webapp/js/controller/itemCatController.js
@@ -55,12 +55,12 @@ app.controller('itemCatController' ,function($scope,$controller   ,itemCatServic
 	
 	 
 	//批量删除 
-	$scope.dele=function(){			
+	$scope.dele=function(){
 		//获取选中的复选框			
 		itemCatService.dele( $scope.selectIds ).success(
 			function(response){
 				if(response.success){
-					$scope.reloadList();//刷新列表
+					$scope.findByParentId($scope.parentId);//刷新列表
 					$scope.selectIds=[];
 				}						
 			}		
