c4c807a75e5ff17dfe0c2d3bb41dfebae2d4beec
FallingSkies <635025373@qq.com>
2018-12-12 15:45:20 +0800

    0.0.98461 新增前端-上传文件功能的显示

--- pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService){	
+app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService){
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -73,5 +73,19 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService){
 			}			
 		);
 	}
+
+
+    //上传图片
+    $scope.uploadFile=function(){
+        uploadService.uploadFile().success(
+            function(response){
+                if(response.success){
+                    $scope.image_entity.url= response.message;
+                }else{
+                    alert(response.message);
+                }
+            }
+        );
+    }
     
 });	
