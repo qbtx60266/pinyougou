04648070f733e3c9c15be4633c133403bd634ba2
FallingSkies <635025373@qq.com>
2018-12-13 17:31:08 +0800

    0.0.98480 新增前端，规格变量创建

--- pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
@@ -164,4 +164,32 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 		}
     }
 
+
+    //创建SKU列表
+    $scope.createItemList=function () {
+		//列表初始化
+		$scope.entity.itemList=[{spec:{},price:0,num:99999,status:'0',isDefault:'0'}];
+		var items=$scope.entity.goodsDesc.specificationItems;
+		for (var i = 0; i <items.length;i ++){
+            $scope.entity.itemList=addColumn($scope.entity.itemList,items[i].attributeName,items[i].attributeValue);
+		}
+    };
+
+
+    addColumn=function (list,columnName,columnValues) {
+		var newList=[];
+
+		for (var i = 0 ; i < list.length;i ++){
+			var oldRow = list[i];
+			for (var j = 0;j < columnValues.length; j ++){
+				//深克隆
+				var newRow = JSON.parse(JSON.stringify(oldRow));
+				newRow.spec[columnName] = columnValues[j];
+				newList.push(newRow);
+			}
+		}
+
+		return newList;
+    }
+
 });
