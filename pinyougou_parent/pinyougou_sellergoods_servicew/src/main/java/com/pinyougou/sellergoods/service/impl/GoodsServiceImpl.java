d6481c3ef65a35d5bb40ece14281108a14573a75
FallingSkies <635025373@qq.com>
2018-12-13 20:12:51 +0800

    0.0.98500 新增后端，SKU列表保存

--- pinyougou_parent/pinyougou_sellergoods_servicew/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou_parent/pinyougou_sellergoods_servicew/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -1,13 +1,17 @@
 package com.pinyougou.sellergoods.service.impl;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-import com.pinyougou.mapper.TbGoodsDescMapper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.pinyougou.mapper.*;
+import com.pinyougou.pojo.TbItem;
 import com.pinyougou.pojogroup.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import com.pinyougou.mapper.TbGoodsMapper;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsExample;
 import com.pinyougou.pojo.TbGoodsExample.Criteria;
@@ -27,6 +31,14 @@ public class GoodsServiceImpl implements GoodsService {
 	private TbGoodsMapper goodsMapper;
 	@Autowired
 	private TbGoodsDescMapper goodsDescMapper;
+	@Autowired
+	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+	@Autowired
+	private TbBrandMapper brandMapper;
+	@Autowired
+	private TbSellerMapper sellerMapper;
 
 	
 	/**
@@ -56,6 +68,38 @@ public class GoodsServiceImpl implements GoodsService {
 		goodsMapper.insert(goods.getGoods());
 		goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());
 		goodsDescMapper.insert(goods.getGoodsDesc());
+		List<TbItem> itemList = goods.getItemList();
+		for (TbItem item : itemList) {
+			//构建标题 SPU名称+ 规格选项值
+			String title=goods.getGoods().getGoodsName();
+			Map<String,Object> map = JSON.parseObject(item.getSpec());
+			for (String key : map.keySet()) {
+				title += " " + map.get(key);
+			}
+			item.setTitle(title);
+			//商品分类
+			item.setCategoryid(goods.getGoods().getCategory3Id());
+			//创建日期
+			item.setCreateTime(new Date());
+			//更新日期
+			item.setUpdateTime(new Date());
+			//商品id
+			item.setGoodsId(goods.getGoods().getId());
+			//卖家id
+			item.setSeller(goods.getGoods().getSellerId());
+			//分类名称
+			item.setCategory(itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id()).getName());
+			//商品名
+			item.setBrand(brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId()).getName());
+			//卖家店铺名
+			item.setSeller(sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId()).getNickName());
+			//图片地址
+			List<Map> imageList = JSON.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
+			if (imageList.size()>0){
+				item.setImage((String)imageList.get(0).get("url"));
+			}
+			itemMapper.insert(item);
+		}
 	}
 
 	
