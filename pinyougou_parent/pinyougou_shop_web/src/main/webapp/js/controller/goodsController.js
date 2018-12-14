8677d4ab87ea040a5424b6bac3b01c44acfce1fa
FallingSkies <635025373@qq.com>
2018-12-14 11:52:09 +0800

    0.0.98505 新增前端，商品分类显示

--- pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou_parent/pinyougou_shop_web/src/main/webapp/js/controller/goodsController.js
@@ -194,5 +194,15 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 
 
     $scope.status=['未审核','已审核','审核未通过','已关闭'];
+	//商品分类列表
+    $scope.itemCatList=[];
+
+    $scope.findItemCatList=function () {
+		itemCatService.findAll().success(function (response) {
+			for (var i = 0; i <response.length;i ++){
+				$scope.itemCatList[response[i].id]=response[i].name;
+			}
+        })
+    }
 
 });
