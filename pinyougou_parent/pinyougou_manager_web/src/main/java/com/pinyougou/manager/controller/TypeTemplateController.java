dd88a67ede2ac6e3ec6caec3ba2f434a46c40332
FallingSkies <635025373@qq.com>
2018-12-16 09:39:18 +0800

    0.0.98524 新增前端:商品审核页bug修复; 新增后端:商品审核页bug修复; 缺少功能:无; bug:注解式事务未测试。

--- pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/TypeTemplateController.java
+++ pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/TypeTemplateController.java
@@ -1,5 +1,6 @@
 package com.pinyougou.manager.controller;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -101,7 +102,7 @@ public class TypeTemplateController {
 	
 		/**
 	 * 查询+分页
-	 * @param brand
+	 * @param typeTemplate
 	 * @param page
 	 * @param rows
 	 * @return
@@ -110,5 +111,16 @@ public class TypeTemplateController {
 	public PageResult search(@RequestBody TbTypeTemplate typeTemplate, int page, int rows  ){
 		return typeTemplateService.findPage(typeTemplate, page, rows);		
 	}
+
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findSpecList")
+	public List<Map> findSpecList(Long id){
+		return typeTemplateService.findSpecList(id);
+	}
+
 	
 }
