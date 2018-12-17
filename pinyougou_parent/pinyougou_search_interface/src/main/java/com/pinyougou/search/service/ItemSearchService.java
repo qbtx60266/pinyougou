a2134ff9fcc2d31bbe22ce53e9b70248d8414dc2
FallingSkies <635025373@qq.com>
2018-12-17 17:11:02 +0800

    0.0.98542 新增前端:无; 新增后端:搜索后台准备工作; 缺少功能:无; bug:注解式事务未测试。

---
+++ pinyougou_parent/pinyougou_search_interface/src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -0,0 +1,17 @@
+package com.pinyougou.search.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author FallingSkies
+ * @date 2018/12/17 16:19
+ */
+public interface ItemSearchService {
+    /**
+     * 搜索方法
+     * @param searchMap
+     * @return
+     */
+    public Map search(Map searchMap);
+}
