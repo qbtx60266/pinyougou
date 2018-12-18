9f7b80383a7d0c7a65c142bf9f9f0e23dd3c6f73
FallingSkies <635025373@qq.com>
2018-12-18 19:50:15 +0800

    0.0.985674 新增前端:搜索对象结构建立; 新增后端:无; 缺少功能:无; bug:注解式事务未测试。

--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,10 @@
 app.controller("searchController",function ($scope,searchService) {
+
+    //定义搜索对象的结构
+    $scope.searchMap={'keyword':'','category':'','brand':'','spec':{}};
+
+
+
     //搜索
     $scope.search=function () {
         searchService.search($scope.searchMap).success(function (response) {
