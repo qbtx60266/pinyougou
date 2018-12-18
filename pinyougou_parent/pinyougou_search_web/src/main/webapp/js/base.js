e9fd1bf5001b352da6671260b9b0cfa179ac4fd8
FallingSkies <635025373@qq.com>
2018-12-18 11:41:53 +0800

    0.0.98547 新增前端:高亮显示; 新增后端:无; 缺少功能:无; bug:注解式事务未测试。

--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/base.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/base.js
@@ -1,1 +1,10 @@
 var app = angular.module("pinyougou",[]);
+
+//定义过滤器
+app.filter('trustHtml',['$sce',function ($sce) {
+    //传入参数为被过滤的内容
+    return function (data) {
+        // 返回过滤后的内容(信任html的转换)
+        return $sce.trustAsHtml(data);
+    }
+}]);
