0d088063aa2a5c8db81d4eddbf0bb4fd3f1e9bec
FallingSkies <635025373@qq.com>
2018-12-19 16:10:36 +0800

    0.0.985685 新增前端:前端分页; 新增后端:无; 缺少功能:无; bug:注解式事务未测试。

--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller("searchController",function ($scope,searchService) {
 
     //定义搜索对象的结构
-    $scope.searchMap={'keyword':'','category':'','brand':'','spec':{},'price':''};
+    $scope.searchMap={'keyword':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':40};
 
 
 
@@ -9,8 +9,37 @@ app.controller("searchController",function ($scope,searchService) {
     $scope.search=function () {
         searchService.search($scope.searchMap).success(function (response) {
             $scope.resultMap=response;
+            buildPageLabel();
         })
     }
+
+    //构建分页栏
+    buildPageLabel=function () {
+        //构建分页栏
+        $scope.pageLabel=[];
+        var firstPage=1;
+        var lastPage=$scope.resultMap.totalPages;
+
+        //总页数大于7
+        if ($scope.resultMap.totalPages > 7){
+            if ($scope.searchMap.pageNo <= 4){
+                lastPage = 7;
+            }else if ($scope.searchMap.pageNo > $scope.resultMap.totalPages - 3){
+                firstPage = $scope.resultMap.totalPages - 6;
+            }else {
+                firstPage = $scope.searchMap.pageNo - 3;
+                lastPage = $scope.searchMap.pageNo + 3;
+            }
+        }
+
+        for (var i = firstPage; i <= lastPage; i ++){
+            $scope.pageLabel.push(i);
+        }
+    }
+
+
+
+
 
     //添加搜索项方法
     $scope.addSearchItem=function (key, value) {
