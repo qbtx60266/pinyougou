40d160df51d93b266439a35c2d78b6f17bc18ce6
FallingSkies <635025373@qq.com>
2018-12-25 10:19:55 +0800

    0.0.0.985916 新增前端:购物车合计数; 新增后端:无; 缺少功能:无; bug:。

--- pinyougou_parent/pinyougou_cart_web/src/main/webapp/js/controller/cartController.js
+++ pinyougou_parent/pinyougou_cart_web/src/main/webapp/js/controller/cartController.js
@@ -1,9 +1,14 @@
 app.controller('cartController',function($scope,cartService){
+
+    $scope.totalValue={totalNum:0,totalMoney:0};
+
+
 //查询购物车列表
     $scope.findCartList=function(){
         cartService.findCartList().success(
             function(response){
                 $scope.cartList=response;
+                $scope.totalValue=cartService.sum($scope.cartList);
             }
         );
     }
@@ -18,6 +23,25 @@ app.controller('cartController',function($scope,cartService){
             }
         })
     }
+    // //合计
+    // sum=function () {
+    //     //总数量
+    //     $scope.totalNum=0;
+    //     //总金额
+    //     $scope.totalFee=0;
+    //     for (var i = 0 ; i < $scope.cartList.length; i ++){
+    //         //购物车对象
+    //         var cart = $scope.cartList[i];
+    //         for (var j = 0 ; j < cart.orderItemList.length; j ++){
+    //             //购物车明细
+    //             var orderItem=  cart.orderItemList[i]
+    //             //累加数量
+    //             $scope.totalNum+=orderItem.num;
+    //             //累加金额
+    //             $scope.totalMoney+=orderItem.totalFee;
+    //         }
+    //     }
+    // }
 
 
 });
